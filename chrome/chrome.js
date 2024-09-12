f3f5c921a6b4a7896f4a7e0e596d7b3525358e98
Het <145864532+Aizenn99@users.noreply.github.com>
2024-09-12 11:11:29 +0000

    changes in js

--- chrome/chrome.js
+++ chrome/chrome.js
@@ -1,21 +1,42 @@
 let myleads = []
-const inputel = document.getElementById("input.el")
+const inputel = document.getElementById("input-el")
 const inputbtn = document.getElementById("input-btn")
-const ulel = document.getElementById("ul.el")
-let inputid = document.getElementById("input-btn")
+const ulel = document.getElementById("ul-el")
+const deletebtn = document.getElementById("delete-btn")
 
-inputid.addEventListener("click",function(){
+let myleadsfromlocalstorage = JSON.parse(localStorage.getItem("myleads"))
+console.log(myleadsfromlocalstorage)
+
+// Check if there are leads in localStorage and render them
+if (myleadsfromlocalstorage) {
+    myleads = myleadsfromlocalstorage
+    renderlist()
+}
+
+// Event listener for the button click
+inputbtn.addEventListener("click", function () {
     myleads.push(inputel.value)
     inputel.value = ""
+
+    // Save the leads to localStorage
+    localStorage.setItem("myleads", JSON.stringify(myleads))
     renderlist()
 })
+
+deletebtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+     myleads = []
+    renderlist()
+})
+
+// Function to render the list of leads
 function renderlist() {
-let listitems = ""
-for (i=0 ;i <myleads.length;i++){
-    listitems +=` <li>
-     <a href=' target=' _blank'> ${myleads[i]}</a> 
-     </li> `
-   
+    let listitems = ""
+    for (let i = 0; i < myleads.length; i++) {
+        listitems += `<li>
+            <a href='${myleads[i]}' target='_blank'>${myleads[i]}</a>
+        </li>`
+    }
+    ulel.innerHTML = listitems
 }
-ulel.innerHTML = listitems
-}
+
